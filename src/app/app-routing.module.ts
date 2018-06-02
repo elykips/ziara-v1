7d6fb8f27b473f791df52908235fd64dc560a46e
elykips <elkanarop@gmail.com>
2018-06-02 22:16:29 +0000

    redirect to home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { CONTENT_ROUTES } from "./shared/routes/content-layout.routes";
 const appRoutes: Routes = [
   {
     path: '',
-    redirectTo: 'full-layout',
+    redirectTo: 'auth',
     pathMatch: 'full',
   },
   { path: '', component: FullLayoutComponent, data: { title: 'full Views' }, children: Full_ROUTES },
